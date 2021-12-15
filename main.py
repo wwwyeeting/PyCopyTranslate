09e985590062b6fca70223d0493128a90db62d8b
Yeeting <wwwyeeting@126.com>
2021-12-15 09:14:28 +0800

    KeyboardInterrupt capture and pause system. add output when system end.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # -*- coding: gbk -*-
+from os import system
 from Capture.ClipCapture import ClipCapture
 
 
 if __name__ == '__main__':
-    captor = ClipCapture(0.5)
-    captor.StartTask()
+    try:
+        captor = ClipCapture(0.5)
+        captor.StartTask()
+    except KeyboardInterrupt:
+        print(r"system end.")
+        system("pause")
